c8108c717391a6e64be6084e72fdeade3d63f381
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-04-09 18:15:46 +0200

    Footer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './Component/header/header.component'
-
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+// Main Components
+import { HeaderComponent } from './Component/header/header.component';
+import { FooterComponent } from './Component/footer/footer.component'
+
+//Shared Components
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -24,4 +29,5 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
